2ce20ca3ef1182dc6618812bb7dacaf01569ec57
Денис Басараб <denisbasarab@MacBook-Pro-Denis.local>
2025-11-14 07:58:43 +0200

    test

--- resources/js/Components/WelcomeBlock.vue
+++ resources/js/Components/WelcomeBlock.vue
@@ -12,7 +12,7 @@
         class="absolute top-1/2 left-1/2 mx-auto flex w-11/12 max-w-[1200px] -translate-x-1/2 -translate-y-1/2 flex-col text-center text-white lg:ml-5"
       >
         <h1 class="text-4xl font-bold sm:text-5xl lg:text-left">
-          Best Collection for Home decoration
+          Best Collection for Home test
         </h1>
         <p class="pt-3 text-xs lg:w-3/5 lg:pt-5 lg:text-left lg:text-base">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
