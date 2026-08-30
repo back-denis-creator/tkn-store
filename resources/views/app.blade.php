6f88532739aad4e847a451352ec447b107a3158f
Денис Басараб <denisbasarab@MacBook-Pro-Denis.local>
2026-08-30 16:50:10 +0300

    add google ver

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,6 +17,8 @@
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 
+        <meta name="google-site-verification" content="pldN7psW9SEH-GBnhiTwl4T1nn96KjS4Fdnyv5XwAQU" />
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
