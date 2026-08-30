b33a2c6ff2f926fbf1be32c95a1f8f2cda18f9fd
Денис Басараб <denisbasarab@MacBook-Pro-Denis.local>
2026-08-30 19:26:35 +0300

    update

--- resources/js/Pages/Profile/Index.vue
+++ resources/js/Pages/Profile/Index.vue
@@ -43,101 +43,85 @@ const tabLinkClass = (tab) => activeTab.value === tab
                     </div>
                 </div>
 
-                <div class="flex border-b py-5">
-                    <div class="flex w-full">
-                        <div class="flex flex-col gap-2">
-                            <button type="button" @click="activeTab = 'info'" :class="tabLinkClass('info')">
-                                {{ $t('Profile Information') }}
-                            </button>
-                            <button type="button" @click="activeTab = 'password'" :class="tabLinkClass('password')">
-                                {{ $t('Change Password') }}
-                            </button>
-                        </div>
-                    </div>
+                <div class="flex flex-col gap-2 border-b py-5">
+                    <button type="button" @click="activeTab = 'info'" :class="tabLinkClass('info')">
+                        {{ $t('Profile Information') }}
+                    </button>
+                    <button type="button" @click="activeTab = 'password'" :class="tabLinkClass('password')">
+                        {{ $t('Change Password') }}
+                    </button>
                 </div>
 
-                <div class="flex border-b py-5">
-                    <div class="flex w-full">
-                        <div class="flex flex-col gap-2">
-                            <Link
-                                :href="route('orders.mine')"
-                                class="flex items-center gap-2 font-medium hover:text-amber-600"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    fill="currentColor"
-                                    class="h-5 w-5"
-                                >
-                                    <path
-                                        d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375z"
-                                    />
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M3.087 9l.54 9.176A3 3 0 006.62 21h10.757a3 3 0 002.995-2.824L20.913 9H3.087zm6.163 3.75A.75.75 0 0110 12h4a.75.75 0 010 1.5h-4a.75.75 0 01-.75-.75z"
-                                        clip-rule="evenodd"
-                                    />
-                                </svg>
-                                {{ $t('Order') }}
-                            </Link>
-                        </div>
-                    </div>
+                <div class="flex flex-col gap-2 border-b py-5">
+                    <Link
+                        :href="route('orders.mine')"
+                        class="flex items-center gap-2 font-medium hover:text-amber-600"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            class="h-5 w-5"
+                        >
+                            <path
+                                d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375z"
+                            />
+                            <path
+                                fill-rule="evenodd"
+                                d="M3.087 9l.54 9.176A3 3 0 006.62 21h10.757a3 3 0 002.995-2.824L20.913 9H3.087zm6.163 3.75A.75.75 0 0110 12h4a.75.75 0 010 1.5h-4a.75.75 0 01-.75-.75z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+                        {{ $t('Order') }}
+                    </Link>
                 </div>
 
-                <div v-if="user.role === 'admin'" class="flex border-b py-5">
-                    <div class="flex w-full">
-                        <div class="flex flex-col gap-2">
-                            <Link
-                                :href="route('dashboard')"
-                                class="flex items-center gap-2 font-medium hover:text-amber-600"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                    stroke="currentColor"
-                                    class="h-5 w-5"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
-                                    />
-                                </svg>
-                                Панель адміністратора
-                            </Link>
-                        </div>
-                    </div>
+                <div v-if="user.role === 'admin'" class="flex flex-col gap-2 border-b py-5">
+                    <Link
+                        :href="route('dashboard')"
+                        class="flex items-center gap-2 font-medium hover:text-amber-600"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="h-5 w-5"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
+                            />
+                        </svg>
+                        Панель адміністратора
+                    </Link>
                 </div>
 
-                <div class="flex py-5">
-                    <div class="flex w-full">
-                        <div class="flex flex-col gap-2">
-                            <Link
-                                :href="route('logout')"
-                                method="post"
-                                as="button"
-                                class="flex items-center gap-2 font-medium hover:text-amber-600"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                    stroke="currentColor"
-                                    class="h-5 w-5"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
-                                    />
-                                </svg>
-                                {{ $t('Log Out') }}
-                            </Link>
-                        </div>
-                    </div>
+                <div class="flex flex-col gap-2 py-5">
+                    <Link
+                        :href="route('logout')"
+                        method="post"
+                        as="button"
+                        class="flex items-center gap-2 font-medium hover:text-amber-600"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="h-5 w-5"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+                            />
+                        </svg>
+                        {{ $t('Log Out') }}
+                    </Link>
                 </div>
             </section>
             <!-- /sidebar  -->
