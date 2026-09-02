76d9615a2503a4408c619b06127aaf9a1d26a9a9
Денис Басараб <denisbasarab@MacBook-Pro-Denis.local>
2026-09-02 23:41:50 +0300

    update

--- app/Models/AttributeOption.php
+++ app/Models/AttributeOption.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Spatie\Image\Enums\Fit;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class AttributeOption extends Model implements HasMedia
 {
@@ -19,6 +21,13 @@ class AttributeOption extends Model implements HasMedia
         'value',
         'meta',
     ];
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('preview')
+            ->fit(Fit::Contain, 300, 300);
+    }
 
     const COLOR_GROUPS = [
         ['id' => 0, 'name' => 'Однотон'],
