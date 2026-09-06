f581ebb96c3939c50ca7228cf670b6aaf44216d5
Денис Басараб <denisbasarab@MacBook-Pro-Denis.local>
2026-09-06 12:38:40 +0300

    update

--- resources/js/Pages/Catalog.vue
+++ resources/js/Pages/Catalog.vue
@@ -244,26 +244,6 @@
                                 >
                                     Casanel
                                 </div>
-                                <div
-                                    class="pointer-events-none absolute flex h-full w-full items-center justify-center gap-3 opacity-0 duration-150 group-hover:opacity-100"
-                                >
-                                    <span class="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-amber-400">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke-width="1.5"
-                                            stroke="currentColor"
-                                            class="h-4 w-4"
-                                        >
-                                            <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                                            />
-                                        </svg>
-                                    </span>
-                                </div>
                             </div>
                         </Link>
 
